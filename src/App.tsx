3d6aac4a9a297b9182455ac3f1b55587edbb0091
jaydeep <jaydeepb.albiorix@gmail.com>
2022-05-24 19:08:36 +0530

    comments resolve

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Registration from "./Components/Registration";
 
 const App = () => {
+  const BASE_URL = process.env.REACT_APP_API   
+
   const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: "http://127.0.0.1:4000/graphql",
+    uri: BASE_URL,
   });
 
   return (
--- src/Components/Registration/index.tsx
+++ src/Components/Registration/index.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import { Form, Input, Button, Card } from "antd";
 import { useMutation } from "@apollo/client";
 import authMutation from "../../gql/Mutation";
+import { registrationSchema } from "../../Helper/Schema/registrationSchema";
 
+const yupSync = {
+  async validator({ field }: any, value: any) {
+    console.log("field,", field);
 
+    registrationSchema.validateSyncAt(field, { [field]: value });
+  },
+};
 function Registration() {
   const [createUser] = useMutation(authMutation.CREATE_USER_MUTATION);
 
@@ -22,31 +29,18 @@ function Registration() {
           name="basic"
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
           onFinish={onFinish}
           onFinishFailed={onFinishFailed}
           autoComplete="off"
         >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: "Please input your username!" }]}
-          >
+          <Form.Item label="Username" name="username" rules={[yupSync]}>
             <Input />
           </Form.Item>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: "Please input your email!" }]}
-          >
+          <Form.Item label="Email" name="email" rules={[yupSync]}>
             <Input />
           </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
+          <Form.Item label="Password" name="password" rules={[yupSync]}>
             <Input.Password />
           </Form.Item>
 
