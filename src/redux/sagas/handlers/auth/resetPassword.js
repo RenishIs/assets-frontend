a003a1c7c20244d22a27fe0587b035dd73274b90
BlessyAlbiorix <blessyf.albiorix@gmail.com>
2022-06-17 11:48:58 +0530

    Toaster message after resetting password, redirect to login.

--- src/redux/sagas/handlers/auth/resetPassword.js
+++ src/redux/sagas/handlers/auth/resetPassword.js
@@ -2,11 +2,14 @@ import { call, put } from "redux-saga/effects";
 import { resetPasswordError, resetPasswordSuccess } from "../../../actions/auth/resetPassword";
 import resetPasswordRequest from "../../requests/auth/resetPassword";
 import openNotificationWithIcon from '../../../../Helper/Notification';
+import { push } from "connected-react-router";
 
 function* handlerResetPassword({payload}){
     try{
         const { data } = yield call(resetPasswordRequest, payload)
+        openNotificationWithIcon('success', 'Password Reset Successful!!')
         yield put(resetPasswordSuccess(data))
+        yield put(push('/login'))
     }
     catch(err){
         openNotificationWithIcon('error', err.message)
