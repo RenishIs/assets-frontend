9ce6ff2367b6aaa64095062a3acc89c34b8b6594
BlessyAlbiorix <blessyf.albiorix@gmail.com>
2022-06-29 18:40:18 +0530

    dropdown role

--- src/Pages/Users/UsersForm.js
+++ src/Pages/Users/UsersForm.js
@@ -1,12 +1,30 @@
-import { Button } from 'antd';
+import { Button, Select, Dropdown, Menu } from 'antd';
 import { UserOutlined, MailFilled, PhoneFilled} from '@ant-design/icons';
-import { Formik, Form } from 'formik';
+import { Formik, Form, useField } from 'formik';
 import TextInput from '../../Components/UI/TextInput';
 import { userValidations } from '../../Helper/ValidationSchema';
 import Dashboard from '../Dashboard';
 import { Row, Col } from 'antd';
 import { KeyOutlined, EnvironmentFilled } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+
+const { Option } = Select;
+const options = ['Admin', 'Employee']
+
+const MySelect = ({ label, ...props }) => {
+    const [field, meta] = useField(props);
+    return (
+        <div className='mt-4 w-full'>
+            <div className='text-start ms-4 mb-1'>
+                <label htmlFor={props.id || props.name} className="text-body text-start fs-6 fw-bold">{label}</label>
+            </div>
+            <Select {...field} {...props} style={{width : '100%'}}/>
+            {meta.touched && meta.error ? (
+            <div className="error">{meta.error}</div>
+            ) : null}
+      </div>
+    );
+};
 
 const UsersForm = ({title, handleUser, ...rest}) => {
 
@@ -62,11 +80,20 @@ const UsersForm = ({title, handleUser, ...rest}) => {
                         </Row>
                         <Row>
                             <Col span={12}>
-                                <TextInput label="ROLE" 
-                                           name="role" 
-                                           id="role"
-                                           prefix={<UserOutlined style={{color : 'black'}}/>}
-                                           isLabel={true}/>
+                                {/* <Select id="role" name="role" defaultValue={options[0]}  style={{width : '100%'}}>
+                                {
+                                    options.map(option => (
+                                        <Option value={options} key={option}>{option}</Option>
+                                    ))
+                                }
+                                </Select> */}
+                                <MySelect name="role" id="role" label="ROLE">
+                                {
+                                    options.map(option => (
+                                        <Option value={option}>{option}</Option>
+                                    ))
+                                }
+                                </MySelect>
                             </Col>
                             <Col span={12}>
                                 <TextInput label="PASSWORD" 
