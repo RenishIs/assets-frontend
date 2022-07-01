eb91475bfd7087a903442ec04480df82125c6821
unknown <nidhib.albiorix@gmail.com>
2022-07-01 20:32:00 +0530

    fixed redirection issue

--- src/Components/UI/SideNavbar.js
+++ src/Components/UI/SideNavbar.js
@@ -1,48 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout, Menu } from 'antd';
 import { UserOutlined, HomeFilled, LaptopOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 const { Sider } = Layout;
-
+function getItem(label, key, icon, path) {
+    return {
+        label,
+        key,
+        icon,
+        path,
+    };
+}
 const iconsData = [
-    { icon : HomeFilled, label : 'Dashboard'},
-    { icon : UserOutlined, label : 'Users', path:'/users'},
-    { icon : LaptopOutlined, label : 'Assets', path:'/assets'},
-    { icon : LaptopOutlined, label : 'Asset Categories', path:'/asset-categories'},
-    { icon : LaptopOutlined, label : 'Asset Types', path:'/asset-types'},
-    { icon : LaptopOutlined, label : 'Asset Status', path:'/asset-status'}
+    getItem("Dashboard", "1", HomeFilled, '/users'),
+    getItem("Assets", "2", UserOutlined, '/assets'),
+    getItem("Asset Categories", "3", LaptopOutlined, '/asset-categories'),
+    getItem("Asset Types", "4", LaptopOutlined, '/asset-types'),
+    getItem("Asset Status", "5", LaptopOutlined, '/asset-status'),
+
 ]
 
 const menuItems = iconsData.map((item, index) => {
-    const key = String(index + 1)
+
     return {
-        key :`sub${key}`,
-        icon : React.createElement(item.icon, {className : 'side-nav-bar-icons',}),
-        label : item.path ? (<Link to={`${item.path}`}>{item.label}</Link>) : item.label,
-        children : item.children && item?.children.map((child, j) => {
-            const subKey = index * 4 + j + 1;
-            return {
-                key : subKey,
-                label : <Link to={`${child.path}`}>{child.label}</Link>
-            }
-        })
+        key: item.key,
+        icon: React.createElement(item.icon, { className: 'side-nav-bar-icons', }),
+        label: item.path ? (<Link to={`${item.path}`}>{item.label}</Link>) : item.label
     }
+
 })
 
-const SideNavbar = ({collapsed}) => {
+const SideNavbar = () => {
+   
+
     return (
-        <Sider width='18%' height="100vh" className='overflow-hidden' collapsible collapsed={collapsed} trigger={null} >
+        <Sider width='18%' height="100vh" className='overflow-hidden' trigger={null} >
             <div className='d-flex justify-content-center align-items-center p-1'>
-                <img src="https://images-platform.99static.com/VjQlwl2IRxelKQzp4tzqY8pD4nY=/500x500/top/smart/99designs-contests-attachments/23/23280/attachment_23280405" alt='pic' style={{width:'50px'}}/>
+                <img src="https://images-platform.99static.com/VjQlwl2IRxelKQzp4tzqY8pD4nY=/500x500/top/smart/99designs-contests-attachments/23/23280/attachment_23280405" alt='pic' style={{ width: '50px' }} />
                 <span className='fw-bold fs-0.5 text-secondary px-4'>Assests Management</span>
-            </div>            
-            <Menu 
+            </div>
+            <Menu
                 theme="dark"
                 mode="inline"
+             
                 defaultSelectedKeys={['1']}
-                className='side-nav-bar-menu-items fs-6'
+             
+       
                 items={menuItems}
-                  />
+            />
         </Sider>
     )
 }
