d36b1ca943a4ce5480d66d00c130db48bcc1a446
BlessyAlbiorix <blessyf.albiorix@gmail.com>
2022-07-07 11:38:59 +0530

    code refactoring

--- src/Pages/Users/UserDetails.js
+++ src/Pages/Users/UserDetails.js
@@ -5,6 +5,21 @@ import { GET_USER_BY_ID_QUERY } from '../../gql/Query/Users';
 import Loader from "../../Components/UI/Loader";
 import { tableColumns } from '../Assets/CONSTANTS';
 
+const RowUI = ({label1, label2, loading, ...rest}) => (
+    <Row className="mb-3">
+        <Col span={12}>
+            <Row span={24}>
+                <div className="text-muted">{label1} : <span className="text-body fw-bold">{rest.value1}</span></div>
+            </Row>
+            </Col>
+            <Col span={12}>
+            <Row span={24}>
+                <div className="text-muted">{label2} : <span className="text-body fw-bold">{rest.value2}</span></div>
+            </Row>
+        </Col>
+    </Row>
+)
+
 const UserDetails = () => {
 
     const { id } = useParams()
@@ -12,56 +27,34 @@ const UserDetails = () => {
         variables : { userByIdId : id }
     })
 
-    if(loading){
-       return <Loader />
-    }
-
-    const {username, email, contactNo, address, assetDetails} = data?.userById
     return (
         <>
+            { loading && <Loader /> } 
             <div className='text-center mb-4'>
                 <h2 className='d-inline fs-5 fw-bold'>USER DETAILS</h2>
             </div>
             <div className="mt-4">
-                <Row className="mb-3">
-                    <Col span={12}>
-                        <Row span={24}>
-                            <div className="text-muted">Username : <span className="text-body fw-bold">{username}</span></div>
-                        </Row>
-                    </Col>
-                    <Col span={12}>
-                        <Row span={24}>
-                            <div className="text-muted">Email : <span className="text-body fw-bold">{email}</span></div>
-                        </Row>
-                    </Col>
-                </Row>
-                <Row className={`${contactNo} || ${address } && 'mb-4'`}>
+                <RowUI label1="Username" 
+                       value1={data?.userById?.username} 
+                       label2="Email" 
+                       value2={data?.userById?.email}
+                       loading={loading}/>
                 {
-                    contactNo && (
-                        <Col span={12}>
-                            <Row span={24}>
-                                <div className="text-muted">Contact Number : <span className="text-body fw-bold">{contactNo}</span></div>
-                            </Row>
-                        </Col>
+                    (data?.userById?.contactNo || data?.userById?.address) && (
+                        <RowUI label1="Contact Number" 
+                               value1={data?.userById?.contactNo} 
+                               label2="Address" 
+                               value2={data?.userById?.address}
+                               loading={loading}/>
                     )
                 }
-                {
-                    address && (
-                        <Col span={12}>
-                            <Row span={24}>
-                                <div className="text-muted">Address : <span className="text-body fw-bold">{address}</span></div>
-                            </Row>
-                        </Col>
-                    )
-                }                    
-                </Row>
             </div>
             <h2 className='d-inline fs-6 fw-bolder text-muted'>ASSETS</h2>
             {
-                assetDetails && (
+                data?.userById?.assetDetails && (
                     <Table bordered 
                            columns={tableColumns} 
-                           dataSource={assetDetails?.map(item => ({...item, key: item.id}))} 
+                           dataSource={data?.userById?.assetDetails?.map(item => ({...item, key: item.id}))} 
                            pagination={false}/>
                 )
             }
