002eed87aa92d3c1e1456a31bbb8a604b5e5dd2e
BlessyAlbiorix <blessyf.albiorix@gmail.com>
2022-07-07 11:14:35 +0530

    Asset Details Screen crash, Asset Details Screen, View button in the table, Loader for asset details

--- src/Pages/Assets/AssetDetails.js
+++ src/Pages/Assets/AssetDetails.js
@@ -2,105 +2,69 @@ import { useParams } from "react-router-dom"
 import { useQuery } from "@apollo/client"
 import { GET_ASSET_BY_ID_QUERY } from "../../gql/Query/Assets"
 import { Row, Col } from 'antd';
+import Loader from "../../Components/UI/Loader";
+import moment from 'moment'
+
+const RowUI = ({label, ...rest}) => (
+    <Row className="mb-3">
+        <Col span={12}>
+            <span className="text-muted">{label} :</span> 
+        </Col>
+        <Col span={12}>
+            <span className="text-body fw-bold">{rest?.value}</span>
+        </Col>
+    </Row>
+)
 
 const AssetDetails = () => {
 
     const { id } = useParams()
 
-    const { data } = useQuery(GET_ASSET_BY_ID_QUERY, {
-		variables: { assetId: id }
+    const { data, loading } = useQuery(GET_ASSET_BY_ID_QUERY, {
+		variables: { assetById: id }
 	});
 
     return (
         <>
-        <div className='text-center mb-4'>
-            <h2 className='d-inline fs-5 fw-bold'>ASSET DETAILS</h2>
-        </div>
-        {
-            data?.assetById &&
-            <div>
-                <Row>
-                    <Col span={8}></Col>
-                    <Col span={12}>
-                        <div className="mt-4 text-start">
-                            <Row className="mb-3">
-                                <Col span={12}>
-                                    <span className="text-muted">Name :</span> 
-                                </Col>
-                                <Col span={12}>
-                                    <span className="text-body fw-bold">{data?.assetById?.name}</span>
-                                </Col>
-                            </Row>
-                            <Row className="mb-3">
-                                <Col span={12}>
-                                    <span className="text-muted">Description :</span> 
-                                </Col>
-                                <Col span={12}>
-                                    <span className="text-body fw-bold">{data?.assetById?.description}</span>
-                                </Col>
-                            </Row>
-                            <Row className="mb-3">
-                                <Col span={12}>
-                                    <span className="text-muted">Location :</span> 
-                                </Col>
-                                <Col span={12}>
-                                    <span className="text-body fw-bold">{data?.assetById?.name}</span>
-                                </Col>
-                            </Row>
-                            <Row className="mb-3">
-                                <Col span={12}>
-                                    <span className="text-muted">Asset Category :</span> 
-                                </Col>
-                                <Col span={12}>
-                                    <span className="text-body fw-bold">{data?.assetById?.name}</span>
-                                </Col>
-                            </Row>
-                            <Row className="mb-3">
-                                <Col span={12}>
-                                    <span className="text-muted">Type :</span> 
-                                </Col>
-                                <Col span={12}>
-                                    <span className="text-body fw-bold">{data?.assetById?.__typename}</span>
-                                </Col>
-                            </Row>
-                            <Row className="mb-3">
-                                <Col span={12}>
-                                    <span className="text-muted">Purchased On :</span> 
-                                </Col>
-                                <Col span={12}>
-                                    <span className="text-body fw-bold">{data?.assetById?.name}</span>
-                                </Col>
-                            </Row>
-                            <Row className="mb-3">
-                                <Col span={12}>
-                                    <span className="text-muted">Asset Condition :</span> 
-                                </Col>
-                                <Col span={12}>
-                                    <span className="text-body fw-bold">{data?.assetById?.name}</span>
-                                </Col>
-                            </Row>
-                            <Row className="mb-3">
-                                <Col span={12}>
-                                    <span className="text-muted">Status :</span> 
-                                </Col>
-                                <Col span={12}>
-                                    <span className="text-body fw-bold">{data?.assetById?.name}</span>
-                                </Col>
-                            </Row>
-                            <Row className="mb-3">
-                                <Col span={12}>
-                                    <span className="text-muted">Date of Asset Assignment :</span> 
-                                </Col>
-                                <Col span={12}>
-                                    <span className="text-body fw-bold">{data?.assetById?.name}</span>
-                                </Col>
-                            </Row>
-                        </div>
-                    </Col>
+            { loading && <Loader /> }
+            <div className='text-center mb-4'>
+                <h2 className='d-inline fs-5 fw-bold'>ASSET DETAILS</h2>
+            </div>
+            <Row>
+                <Col span={8}></Col>
+                <Col span={12}>
+                    <div className="mt-4 text-start">
+                        <RowUI label="Name" value={data?.assetById?.name}/>
+                        { 
+                            data?.assetById?.description && (
+                                <RowUI label="Description" value={data?.assetById?.description}/>
+                            )
+                        }
+                        <RowUI label="Location" value={data?.assetById?.location}/>
+                        <RowUI label="Asset Category" value={data?.assetById?.assetCategory?.name}/>
+                        <RowUI label="Asset Type" value={data?.assetById?.assetType?.name}/>
+                        {
+                            data?.assetById?.purchasedOn && (
+                                <RowUI label="Purchased On" value={moment(data?.assetById?.purchasedOn).format("Do MM YYYY")}/>
+                            )
+                        }
+                        <RowUI label="Asset Condition" value={data?.assetById?.assetCondition}/>
+                        <RowUI label="Asset Status" value={data?.assetById?.assetStatus?.name}/>
+                        { 
+                            data?.assetById?.reason && (
+                                <RowUI label="Reason" value={data?.assetById?.reason}/>
+                            )
+                        }
+                        <RowUI label="Employee" value={data?.assetById?.employeeId?.username}/>
+                        {   data?.assetById?.dateOfAssetAssignment && (
+                                <RowUI label="Date of Asset Assignment" value={moment(data?.assetById?.dateOfAssetAssignment).format("MMMM Do YYYY")}/>
+                            )
+                        }
+                    </div>
+                </Col>
                 <Col span={4}></Col>
             </Row>
-        </div>}
-    </>
+        </>
     )
 }
 
--- src/Pages/Assets/index.js
+++ src/Pages/Assets/index.js
@@ -5,7 +5,7 @@ import { useMutation, useQuery } from '@apollo/client';
 import { GET_ASSETS_QUERY } from '../../gql/Query/Assets';
 import { DELETE_ASSET_MUTATION } from '../../gql/Mutation/Assets';
 import openNotificationWithIcon from '../../Helper/Notification';
-import { EditFilled, DeleteFilled } from '@ant-design/icons';
+import { EditFilled, DeleteFilled, EyeFilled } from '@ant-design/icons';
 import Loader from '../../Components/UI/Loader';
 
 const confirm = Modal.confirm;
@@ -57,6 +57,7 @@ const AssetsListing = () => {
 					history.push(`/assets/edit/${record.id}`)
 				}}><EditFilled style={{color: "blue"}}/></Link>
 				<DeleteFilled style={{color: "red"}} onClick={(e) => showDeleteConfirm(e, record.id)}/>
+				<Link to={`/assets/${record.id}`}><EyeFilled style={{color:"green"}}/></Link>
 			</Space>
 		),
 	}]
