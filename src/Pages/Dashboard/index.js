9ec376397f3a0ae56f86d1d8dfc7072e69275bb1
BlessyAlbiorix <blessyf.albiorix@gmail.com>
2022-07-11 17:18:32 +0530

    code cleanup

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,69 +1,7 @@
-import React, { useState } from 'react';
-import { Layout, Row, Dropdown, Menu, Col } from 'antd';
-import { MenuFoldOutlined, MenuUnfoldOutlined, UserAddOutlined, UserOutlined, LogoutOutlined } from '@ant-design/icons';
-import SideNavbar from '../../Components/UI/SideNavbar';
-import { Link, useHistory } from 'react-router-dom';
-import Cookies from "js-cookie";
-
-const { Header } = Layout;
-
-const Dashboard = ({children, routes, path}) => {
-    const [collapsed, setCollapsed] = useState(false);
-    const history = useHistory()
-
-    const user= Cookies.get('user')
-
-    const logoutUser = () => {
-        Cookies.remove('token')
-        Cookies.remove('role')
-        history.push('/login')
-    }
-
-    const menu = (
-        <Menu items={[
-            {
-                label: <Link to="/profile" className="text-body"><UserOutlined style={{color:"blue"}} />&nbsp;Profile</Link>,
-                key: '0',
-            },
-            {
-                label: <div onClick={logoutUser}><LogoutOutlined style={{color:"red"}} />&nbsp;Logout</div>,
-                key: '1',
-            },
-          ]}
-        />
-    );
-
+const Dashboard = () => {
+    
     return (
-        <Layout style={{height : '100vh'}}>
-            <Layout>
-                <SideNavbar collapsed={collapsed} routes={routes} path={path}/>
-                <Layout className='overflow-auto'>
-                    <Header className='bg-white mb-4'>
-                        <Row justify="space-between" className='px-4 header' >
-                            <Col>
-                            {
-                            React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                                className : 'trigger fs-4',
-                                onClick: () => setCollapsed(!collapsed)
-                            })
-                            }
-                            </Col>
-                            <Col span={4}>
-                                <Dropdown overlay={menu} trigger={['click']} >
-                                    <div className='text-center d-flex align-items-center justify-content-end fs-5 fw-bolder'>
-                                        <img src="/user-1.png" alt='pic' width="45px" /> &nbsp;
-                                        Hi, {user}
-                                    </div>
-                                </Dropdown>
-                            </Col>
-                        </Row>
-                    </Header>
-                    <div className="text-center p-4 mx-4 main-card mb-4">
-                        {children}
-                    </div>
-                </Layout>
-            </Layout>
-        </Layout>
+        <>hhj</>
     )
 }
 
