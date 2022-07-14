cd9ca612ecda74b595e25320d92fbb521598bf57
BlessyAlbiorix <blessyf.albiorix@gmail.com>
2022-07-14 12:05:01 +0530

    update api for assets in dashboard

--- src/Pages/AssetDashboard/index.js
+++ src/Pages/AssetDashboard/index.js
@@ -50,61 +50,70 @@ const AssetDashboard = () => {
     
     const onDragEnd = (result) => {
         const {destination, source, draggableId } = result;
-        const movedAsset = {...assets?.assets?.find(asset => asset.id === draggableId), assetStatus : assetStatus?.assetStatus.find(status => status.id === destination.droppableId) }
-        
-        if(!destination){
-            return
+        const { id, __typename, ...rest} = assets?.assets?.find(asset => asset.id === draggableId)
+        const updatedMovedAsset = {
+            ...rest,
+             assetStatus : assetStatus?.assetStatus.find(status => status.id === destination.droppableId).id,
+             assetCategory : rest?.assetCategory?.id,
+             assetType : rest?.assetType?.id,
+             employeeId : rest?.employeeId.id
         }
-        if (destination.droppableId === source.droppableId && destination.index === source.index) {
-            return
-        }
-
-        updateAssets({ variables: { updateAssetsId: movedAsset.id, input: { ...movedAsset } } })
-
-        const start = data?.columns[source.droppableId]
-        const finish = data?.columns[destination.droppableId]
+        updateAssets({ variables: { updateAssetsId: id, input: { ...updatedMovedAsset } } })
+        refetchAssetStatus()
+        refetchAssets({ status : null })
         
-        if(start == finish){
-            const newTasks = Array.from(start.tasks)
-            newTasks.splice(source.index, 1)
-            newTasks.splice(destination.index, 0, draggableId)
+        // if(!destination){
+        //     return
+        // }
+        // if (destination.droppableId === source.droppableId && destination.index === source.index) {
+        //     return
+        // }
+
+
+        // const start = data?.columns[source.droppableId]
+        // const finish = data?.columns[destination.droppableId]
+        
+        // if(start == finish){
+        //     const newTasks = Array.from(start.tasks)
+        //     newTasks.splice(source.index, 1)
+        //     newTasks.splice(destination.index, 0, draggableId)
     
-            const newColumn = {
-                ...start,
-                tasks : newTasks
-            }
+        //     const newColumn = {
+        //         ...start,
+        //         tasks : newTasks
+        //     }
     
-            setData((prevState) => ({
-                ...prevState,
-                columns : {
-                    ...prevState.columns,
-                    [newColumn.id] : newColumn
-                }
-            }))
-        }
+        //     setData((prevState) => ({
+        //         ...prevState,
+        //         columns : {
+        //             ...prevState.columns,
+        //             [newColumn.id] : newColumn
+        //         }
+        //     }))
+        // }
         
-        //moving from one list to another
-        const startTasks = Array.from(start.tasks)
-        startTasks.splice(source.index, 1)
-        const newStartColumn = {
-            ...start,
-            tasks : startTasks
-        }
+        // //moving from one list to another
+        // const startTasks = Array.from(start.tasks)
+        // startTasks.splice(source.index, 1)
+        // const newStartColumn = {
+        //     ...start,
+        //     tasks : startTasks
+        // }
 
-        const destinationTasks = Array.from(finish.tasks)
-        destinationTasks.splice(destination.index, 0, draggableId)
-        const newDestincationColumn = {
-            ...finish,
-            tasks : destinationTasks
-        }
-        setData((prevState) => ({
-            ...prevState,
-            columns : {
-                ...prevState.columns,
-                [newStartColumn.id] : newStartColumn,
-                [newDestincationColumn.id] : newDestincationColumn
-            }
-        }))
+        // const destinationTasks = Array.from(finish.tasks)
+        // destinationTasks.splice(destination.index, 0, draggableId)
+        // const newDestincationColumn = {
+        //     ...finish,
+        //     tasks : destinationTasks
+        // }
+        // setData((prevState) => ({
+        //     ...prevState,
+        //     columns : {
+        //         ...prevState.columns,
+        //         [newStartColumn.id] : newStartColumn,
+        //         [newDestincationColumn.id] : newDestincationColumn
+        //     }
+        // }))
     }
 
     return (
