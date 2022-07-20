ca1ecef8659935155e6025a45e1504b9d5b9a946
RiddhiAlbiorix <riddhi.albiorix@gmail.com>
2022-07-20 16:20:22 +0530

    firebase deployment

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { setContext } from '@apollo/client/link/context';
 import Cookies from "js-cookie";
 import { onError } from '@apollo/client/link/error';
 import Routes from './Routing'
+import firebase from 'firebase/compat/app';
 
 const httpLink = createHttpLink({
 	uri: process.env.REACT_APP_BASE_URL,
@@ -11,18 +12,18 @@ const httpLink = createHttpLink({
 const authLink = setContext((_, { headers }) => {
 	const token = Cookies.get('token')
 	return {
-	  headers: {
-		...headers,
-		authorization: token ? `Bearer ${token}` : "",
-	  }
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : "",
+		}
 	}
 });
 
 
-const logoutLink = onError(( error) => {
+const logoutLink = onError((error) => {
 	const graphqlErr = error?.graphQLErrors?.[0]
 	const notAuthorised = graphqlErr?.extensions?.code == "UNAUTHENTICATED" ? true : false
-	if(notAuthorised){
+	if (notAuthorised) {
 		Cookies.remove('token')
 		Cookies.remove('role')
 		window.location.reload()
@@ -35,10 +36,23 @@ export const client = new ApolloClient({
 });
 
 const App = () => {
+	var config = {
+		apiKey: process.env.REACT_APP_API_KEY,
+		// authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+		//projectId: process.env.REACT_APP_PROJECT_ID,
+		//storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+		//messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+		//appId: process.env.REACT_APP_APP_ID,
+		//measurementId: process.env.REACT_APP_MEASUREMENT_ID
+	};
+
+	if (!firebase.apps.length) {
+		firebase.initializeApp(config);
+	}
 	return (
 		<ApolloProvider client={client}>
 			<Routes />
-		</ApolloProvider>		
+		</ApolloProvider>
 	);
 };
 
