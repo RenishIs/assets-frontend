43986e1cde629d475355c6a56aa9b924764ef018
unknown <nidhib.albiorix@gmail.com>
2022-09-14 16:55:05 +0530

    validation message for duplecate asset Id

--- src/Pages/Assets/AssetsAdd.js
+++ src/Pages/Assets/AssetsAdd.js
@@ -1,36 +1,46 @@
-import { useMutation } from '@apollo/client';
-import { useHistory } from 'react-router-dom';
-import { CREATE_ASSET_MUTATION } from '../../gql/Mutation/Assets';
-import { GET_ASSETS_QUERY } from '../../gql/Query/Assets';
-import AssetsForm from './AssetsForm';
-import openNotificationWithIcon from '../../Helper/Notification';
-import { GET_ASSET_STATUS_QUERY } from '../../gql/Query/AssetStatus';
-import { GET_ASSET_TYPES_QUERY } from '../../gql/Query/AssetTypes';
-import { GET_ASSET_CATEGORIES_QUERY } from '../../gql/Query/AssetCategories';
+import { useMutation } from "@apollo/client";
+import { useHistory } from "react-router-dom";
+import { CREATE_ASSET_MUTATION } from "../../gql/Mutation/Assets";
+import { GET_ASSETS_QUERY } from "../../gql/Query/Assets";
+import AssetsForm from "./AssetsForm";
+import openNotificationWithIcon from "../../Helper/Notification";
+import { GET_ASSET_STATUS_QUERY } from "../../gql/Query/AssetStatus";
+import { GET_ASSET_TYPES_QUERY } from "../../gql/Query/AssetTypes";
+import { GET_ASSET_CATEGORIES_QUERY } from "../../gql/Query/AssetCategories";
 
 const AssetsAdd = () => {
-	const history = useHistory();
-	const [addAssets, { data, loading }] = useMutation(CREATE_ASSET_MUTATION, {
-		refetchQueries: [
-			{ query: GET_ASSETS_QUERY, variables : { status: null } },
-			{ query: GET_ASSET_STATUS_QUERY },
-			{ query: GET_ASSET_TYPES_QUERY },
-			{ query : GET_ASSET_CATEGORIES_QUERY}
-		]
-	});
+  const history = useHistory();
+  const [addAssets, { data, error, loading }] = useMutation(
+    CREATE_ASSET_MUTATION,
+    {
+      refetchQueries: [
+        { query: GET_ASSETS_QUERY, variables: { status: null } },
+        { query: GET_ASSET_STATUS_QUERY },
+        { query: GET_ASSET_TYPES_QUERY },
+        { query: GET_ASSET_CATEGORIES_QUERY },
+      ],
+    }
+  );
 
-	if (data) {
-		openNotificationWithIcon('addAssest','success', "Asset added successfully")
-		history.push('/assets');
-	}
+  if (data) {
+    openNotificationWithIcon(
+      "addAssest",
+      "success",
+      "Asset added successfully"
+    );
+    history.push("/assets");
+  }
 
-	const handleAsset = (values) => {
-		addAssets({ variables: { input: { ...values } } });
-	}
+  if (error) {
 
-	return (
-		<AssetsForm handleAsset={handleAsset} loading={loading}/>
-	)
-}
+    openNotificationWithIcon("addAssestError", "error", error.message);
+  }
 
-export default AssetsAdd
+  const handleAsset = (values) => {
+    addAssets({ variables: { input: { ...values } } });
+  };
+
+  return <AssetsForm handleAsset={handleAsset} loading={loading} />;
+};
+
+export default AssetsAdd;
